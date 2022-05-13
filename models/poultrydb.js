d64625d83bddfabb19ea532c788dd566edfc9952
Sean Gillis <seangillis@Seans-MacBook-Pro.local>
2022-05-13 14:39:22 -0500

    added poultry seed data - formatting looking good

---
+++ models/poultrydb.js
@@ -0,0 +1,62 @@
+module.exports = [
+  {
+    name: 'The Best Turkey Burger',
+    img: 'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F43%2F2022%2F03%2F28%2F39748-Actually-Delicious-Turkey-Burgers-mfs-162.jpg',
+    link: 'https://www.allrecipes.com/recipe/39748/actually-delicious-turkey-burgers/',
+    cookTime: 30,
+    type: 'Light and Healthy',
+    base: 'Ground Turkey',
+    genre: 'Burger/Sandwich',
+    comments: ''
+  },
+  {
+    name: 'One Pan Balsamic Chicken',
+    img: 'https://hips.hearstapps.com/del.h-cdn.co/assets/17/29/640x959/gallery-1500414775-delish-one-pan-balsamic-chicken-asparagus-pinterest-2.jpg?resize=980:*',
+    link: 'https://www.delish.com/cooking/recipe-ideas/recipes/a54291/one-pan-balsamic-chicken-and-asparagus-recipe/',
+    cookTime: 20,
+    type: 'Family Staple',
+    base: 'Chicken Tenderloin',
+    genre: 'Low Carb',
+    comments: ''
+  },
+  {
+    name: 'Baked Turkey Meatballs',
+    img: 'https://therecipecritic.com/wp-content/uploads/2020/12/bakedturkeymeatballs.jpg',
+    link: 'https://therecipecritic.com/baked-turkey-meatballs/',
+    cookTime: 30,
+    type: 'Family Staple',
+    base: 'Ground Turkey',
+    genre: 'Home Comfort',
+    comments: ''
+  },
+  {
+    name: 'Chicken Lettuce Wraps',
+    img: 'https://www.tasteandtellblog.com/wp-content/uploads/2021/12/Chicken-Lettuce-Wraps-6.jpg',
+    link: 'https://www.tasteandtellblog.com/asian-chicken-lettuce-wraps/',
+    cookTime: 25,
+    type: 'Light and Healthy',
+    base: 'Ground Chicken',
+    genre: 'Asian-inspired',
+    comments: ''
+  },
+  {
+    name: 'Crispy Fried Chicken',
+    img: 'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2012/11/2/0/DV1510H_fried-chicken-recipe-10_s4x3.jpg.rend.hgtvcom.826.620.suffix/1568222255998.jpeg',
+    link: 'https://www.foodnetwork.com/recipes/fried-chicken-recipe10-3381583',
+    cookTime: 50,
+    type: 'Fried Deliciousness',
+    base: 'Chicken Wings & Thighs',
+    genre: 'Southern',
+    comments: ''
+  },
+  {
+    name: 'Crunchy Baked Turkey Tacos',
+    img: 'https://thedefineddish.com/wp-content/uploads/2019/08/2019-08-06-02.02.10.jpg',
+    link: 'https://thedefineddish.com/crunchy-baked-turkey-tacos/',
+    cookTime: 40,
+    type: 'Healthy Alternative',
+    base: 'Ground Turkey',
+    genre: 'Family Staple',
+    comments: ''
+  },
+]
